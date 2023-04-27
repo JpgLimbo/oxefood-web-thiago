74bbf29c10ed3dc150472683cc511d542fffed39
= <=>
2023-04-27 15:51:15 -0300

    criando as list

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import FormCliente from './views/cliente/FormCliente';
+import ListCliente from './views/cliente/ListCliente';
+import ListEntregador from './views/entregador/ListEntregador';
 import FormEntregador from './views/entregador/FormEntregador';
 import Home from './views/home/Home';
 import FormProduto from './views/produto/FormProduto';
@@ -11,8 +13,10 @@ function Rotas() {
         <>
             <Routes>
                 <Route path="/" element={ <Home/> } />
+                <Route path="list-cliente" element={ <ListCliente/> } />
                 <Route path="form-cliente" element={ <FormCliente/> } />
                 <Route path="form-produto" element={ <FormProduto/> } />
+                <Route path="list-entregador" element={ <ListEntregador/> } />
                 <Route path="form-entregador" element={ <FormEntregador/> } />
                 
             </Routes>
---
+++ src/views/entregador/ListEntregador.jsx
@@ -0,0 +1,148 @@
+import axios from 'axios';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+
+class ListEntregador extends React.Component{
+
+   state = {
+
+       listaEntregadores: []
+      
+    }
+
+   componentDidMount = () => {
+      
+       this.carregarLista();
+      
+    }
+   carregarLista = () => {
+
+    axios.get("http://localhost:8082/api/entregador")
+    .then((response) => {
+       
+        this.setState({
+            listaEntregadores: response.data
+            })
+        })
+
+    };
+
+    formatarData = (dataParam) => {
+
+        if (dataParam == null || dataParam == '') {
+            return ''
+        }
+        
+        let dia = dataParam.substr(8,2);
+        let mes = dataParam.substr(5,2);
+        let ano = dataParam.substr(0,4);
+        let dataFormatada = dia + '/' + mes + '/' + ano;
+
+        return dataFormatada
+    };
+
+
+render(){
+    return(
+        <div>
+
+            <div style={{marginTop: '3%'}}>
+
+                <Container textAlign='justified' >
+
+                    <h2> Entregador </h2>
+
+                    <Divider />
+
+                    <div style={{marginTop: '4%'}}>
+        
+                        <Button
+                            inverted
+                            circular
+                            icon
+                            labelPosition='left'
+                            color='orange'
+                            floated='right'
+                        >
+                            <Icon name='clipboard outline' />
+                            <Link to={'/form-entregador'}>Novo</Link>
+                        </Button>
+                        <br/><br/><br/>
+                      
+                      <Table color='orange' sortable celled>
+
+                          <Table.Header>
+                              <Table.Row>
+                                  <Table.HeaderCell>Nome</Table.HeaderCell>
+                                  <Table.HeaderCell>CPF</Table.HeaderCell>
+                                  <Table.HeaderCell>Rg</Table.HeaderCell>
+                                  <Table.HeaderCell>Data Nascimento</Table.HeaderCell>
+                                  <Table.HeaderCell>Fone Celular</Table.HeaderCell>
+                                  <Table.HeaderCell>Fone Fixo</Table.HeaderCell>
+                                  <Table.HeaderCell>Entregas Realizadas</Table.HeaderCell>
+                                  <Table.HeaderCell>Valor Por Frete</Table.HeaderCell>
+                                  <Table.HeaderCell>Rua</Table.HeaderCell>
+                                  <Table.HeaderCell>Numero</Table.HeaderCell>
+                                  <Table.HeaderCell>Bairro</Table.HeaderCell>
+                                  <Table.HeaderCell>Cidade</Table.HeaderCell>
+                                  <Table.HeaderCell>Cep</Table.HeaderCell>
+                                  <Table.HeaderCell>Uf</Table.HeaderCell>
+                                  <Table.HeaderCell>Complemento</Table.HeaderCell>
+                                  <Table.HeaderCell>ativo</Table.HeaderCell>
+                                  <Table.HeaderCell textAlign='center' width={2}>Ações</Table.HeaderCell>
+                              </Table.Row>
+                          </Table.Header>
+                     
+                          <Table.Body>
+
+                              { this.state.listaEntregadores.map(entregador => (
+
+                                  <Table.Row>
+                                      <Table.Cell>{entregador.nome}</Table.Cell>
+                                      <Table.Cell>{entregador.cpf}</Table.Cell>
+                                      <Table.Cell>{entregador.rg}</Table.Cell>
+                                      <Table.Cell>{this.formatarData(entregador.dataNascimento)}</Table.Cell>
+                                      <Table.Cell>{entregador.foneCelular}</Table.Cell>
+                                      <Table.Cell>{entregador.foneFixo}</Table.Cell>
+                                      <Table.Cell>{entregador.qtdEntregasRealizadas}</Table.Cell>
+                                      <Table.Cell>{entregador.valorFrete}</Table.Cell>
+                                      <Table.Cell>{entregador.enderecoRua}</Table.Cell>
+                                      <Table.Cell>{entregador.enderecoNumero}</Table.Cell>
+                                      <Table.Cell>{entregador.enderecoBairro}</Table.Cell>
+                                      <Table.Cell>{entregador.enderecoCidade}</Table.Cell>
+                                      <Table.Cell>{entregador.enderecoCep}</Table.Cell>
+                                      <Table.Cell>{entregador.enderecoUf}</Table.Cell>
+                                      <Table.Cell>{entregador.enderecoCompleto}</Table.Cell>
+                                      <Table.Cell>{entregador.ativo}</Table.Cell>
+
+                                      <Table.Cell textAlign='center'>
+                                         
+                                          <Button
+                                              inverted
+                                              circular
+                                              icon='edit'
+                                              color='blue'
+                                              itle='Clique aqui para editar os dados deste entregador' /> &nbsp;
+                                             <Button
+                                                   inverted
+                                                   circular
+                                                   icon='trash'
+                                                   color='red'
+                                                   title='Clique aqui para remover este entregador' />
+
+                                        </Table.Cell>
+                                    </Table.Row>
+                                   ))}
+
+                               </Table.Body>
+                           </Table>
+                       </div>
+                   </Container>
+               </div>
+           </div>
+       )
+   }
+}
+
+export default ListEntregador;
